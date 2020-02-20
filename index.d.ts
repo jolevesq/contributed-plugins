79058603e12d675e2ce04085d49c7bebe010e95f
Levesque <jolevesq@nrn.nrcan.gc.ca>
2020-02-20 11:15:22 -0500

    Updates

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,37 @@
-import { PanelManager } from './panel-manager';
-export default class Draw {
+export default class Swiper {
     /**
-     * Plugin init
-     * @function init
-     * @param {Object} mapApi the viewer api
-     */
+    * Plugin init
+    * @function init
+    * @param {Object} mapApi the viewer api
+    */
+    private layerNb;
     init(mapApi: any): void;
     /**
-     * Event to fire on side menu item click
-     * @function onMenuItemClick
-     * @return {function} the function to run
-     */
-    onMenuItemClick(): () => void;
+    * Set the swiper
+    * @function setSwiper
+    * @param {Object} myBundle the ESRI dependecy
+    * @param {Config} swiper the swiper configuration
+    */
+    setSwiper(myBundle: any, swiper: config): void;
+    /**
+    * Closure function to manage variables scope
+    * @function closureFunc
+    * @param {Function} fn function to applyt the closure to
+    * @param {Object[]} params array of variables to set
+    */ s: any;
+    closureFunc: (fn: any, ...params: number[]) => () => any;
 }
-export default interface Draw {
+interface config {
+    type: string;
+    keyboardOffset: number;
+    layers: layer[];
+}
+interface layer {
+    id: string;
+}
+export default interface Swiper {
     mapApi: any;
     _RV: any;
     config: any;
-    translations: any;
-    panelManager: PanelManager;
-    button: any;
 }
+export {};
