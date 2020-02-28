250fd15c6a61caa9989bbc7a07b2be779e100fc7
Johann Levesque <johann.levesque@canada.ca>
2020-02-28 12:19:58 -0500

    Updates

--- thematic-slider/slider-panel.d.ts
+++ thematic-slider/slider-panel.d.ts
@@ -1,5 +1,3 @@
-/// <reference types="jquery" />
-import { Observable, BehaviorSubject } from 'rxjs';
 export declare class SliderPanel {
     private _mapApi;
     private _panelSlider;
@@ -10,16 +8,6 @@ export declare class SliderPanel {
     private _layerNb;
     private _loop;
     private _stack;
-    static _playState: BehaviorSubject<boolean>;
-    static getPlayState(): Observable<boolean>;
-    private static setPlayState;
-    static _description: BehaviorSubject<object>;
-    static getDescription(): Observable<object>;
-    private static setDescription;
-    static _end: BehaviorSubject<string>;
-    static getLastStep(): Observable<string>;
-    private static setLastStep;
-    private readonly active;
     /**
      * Slider panel constructor
      * @constructor
@@ -28,12 +16,12 @@ export declare class SliderPanel {
      */
     constructor(mapApi: any, config: any);
     /**
-     * Compile template to link controller and html
-     * @function compileTemplate
-     * @param {String} template measure control
-     * @return {JQuery<HTMLElement>} temp compile template
-     */
-    compileTemplate(template: any): JQuery<HTMLElement>;
+ * Compile template to link controller and html
+ * @function compileTemplate
+ * @param {String} template measure control
+ * @return {JQuery<HTMLElement>} temp compile template
+ */
+    private compileTemplate;
     /**
      * Open the panel
      * @function open
@@ -50,35 +38,6 @@ export declare class SliderPanel {
      * @param {String} controls the controls to add
      */
     private addControls;
-    /**
-     * Set the panel info with the active layer
-     * @function setPanelInfo
-     */
-    private setPanelInfo;
-    /**
-     * Step the panel information and layer visibility up or down
-     * @function step
-     * @param {String} direction the direction to step
-     * @return {Boolean} true if last or first element of the array, false otherwise
-     */
-    step(direction?: string): boolean;
-    /**
-     * Set layers visibility
-     * @function setLayerVisibility
-     */
-    private setLayerVisibility;
-    /**
-     * Set play or pause state. Play will call step function with up calues at the interval from the configuration
-     * @function play
-     * @param {Boolean} isPlaying state to put the slider to
-     */
-    play(isPlaying: boolean): void;
-    /**
-     * Set play (call step) with the proper interval
-     * @function setPlayInterval
-     * @param {SliderPanel} that sthe slider class to access within the interval function
-     */
-    private setPlayInterval;
 }
 export interface SliderDescription {
     desc: string;
