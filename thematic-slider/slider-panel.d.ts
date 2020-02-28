34208c85667d82b73ccf6cfa0d31fcb00e5269a5
Johann Levesque <johann.levesque@canada.ca>
2020-02-28 15:17:16 -0500

    Updates

--- thematic-slider/slider-panel.d.ts
+++ thematic-slider/slider-panel.d.ts
@@ -1,3 +1,4 @@
+import { Observable, BehaviorSubject } from 'rxjs';
 export declare class SliderPanel {
     private _mapApi;
     private _panelSlider;
@@ -8,6 +9,16 @@ export declare class SliderPanel {
     private _layerNb;
     private _loop;
     private _stack;
+    static _playState: BehaviorSubject<boolean>;
+    static getPlayState(): Observable<boolean>;
+    private static setPlayState;
+    static _description: BehaviorSubject<object>;
+    static getDescription(): Observable<object>;
+    private static setDescription;
+    static _end: BehaviorSubject<string>;
+    static getLastStep(): Observable<string>;
+    private static setLastStep;
+    private readonly active;
     /**
      * Slider panel constructor
      * @constructor
@@ -31,6 +42,11 @@ export declare class SliderPanel {
      * @param {String} controls the controls to add
      */
     private addControls;
+    /**
+     * Set the panel info with the active layer
+     * @function setPanelInfo
+     */
+    private setPanelInfo;
 }
 export interface SliderDescription {
     desc: string;
