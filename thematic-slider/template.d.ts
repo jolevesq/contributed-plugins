e5d327497089a34257b74a9913723dd2c53ec9a8
Johann Levesque <johann.levesque@canada.ca>
2020-02-28 13:45:10 -0500

    Updates

--- thematic-slider/template.d.ts
+++ thematic-slider/template.d.ts
@@ -1,3 +1,3 @@
 export declare const SLIDER_TEMPLATE = "\n<div rv-focus-member class=\"rv-thematic-slider\">\n</div>";
-export declare const DESCRIPTION_TEMPLATE = "\n<div class=\"rv-thslider-desc-control\" ng-controller=\"DescriptionCtrl as ctrl\">\n    <div class=\"rv-thslider-desc\">{{ ctrl.description }}</div>\n</div>";
+export declare const DESCRIPTION_TEMPLATE = "\n<div class=\"\" ng-controller=\"DescriptionCtrl as ctrl\">\n    <div class=\"rv-thslider-desc\">test</div>\n</div>";
 export declare const SAMPLE_BUTTON = "\n<div class=\"\" ng-controller=\"SampleCtrl as ctrl\">\n    <md-button\n        aria-label=\"{{ 'plugins.myFirstPlugin.placeHolder' | translate }}\"\n        class=\"rv-button-square md-button ng-scope md-ink-ripple\"\n        ng-click=\"ctrl.sampleFunction()\">\n            {{ 'plugins.myFirstPlugin.placeHolder' | translate }}\n        <md-tooltip>{{ 'plugins.myFirstPlugin.placeHolder' | translate }}</md-tooltip>\n    </md-button>\n</div>\n";
