fb7e2acedf24b46a9e07309eec49d071a0817dd7
Johann Levesque <johann.levesque@canada.ca>
2020-02-28 08:29:35 -0500

    Updates

--- thematic-slider/index.d.ts
+++ thematic-slider/index.d.ts
@@ -1,6 +1,8 @@
+/// <reference types="jquery" />
 export default class ThematicSlider {
     private _button;
     private _panel;
+    private _panelOptionsForm;
     /**
     * Plugin init
     * @function init
@@ -13,6 +15,8 @@ export default class ThematicSlider {
     * @return {function} the function to run
     */
     onMenuItemClick(): () => void;
+    setAngular(): void;
+    compileTemplate(template: string): JQuery<HTMLElement>;
 }
 export default interface ThematicSlider {
     mapApi: any;
