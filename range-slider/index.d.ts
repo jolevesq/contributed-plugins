70cf4bd585b6df2a79d5e702445e01a3c5d8e889
Johann Levesque <johann.levesque@canada.ca>
2020-03-27 09:26:21 -0400

    Updates

--- range-slider/index.d.ts
+++ range-slider/index.d.ts
@@ -1,17 +1,10 @@
 export default class RangeSlider {
-    private _button;
     /**
     * Plugin init
     * @function init
     * @param {Any} mapApi the viewer api
     */
     init(mapApi: any): void;
-    /**
-    * Event to fire on side menu item click. Open/Close the panel
-    * @function onMenuItemClick
-    * @return {function} the function to run
-    */
-    onMenuItemClick(): () => void;
     /**
      * Set layer properties for added layer
      * @function setLayer
