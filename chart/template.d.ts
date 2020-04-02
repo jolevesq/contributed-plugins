90e7a9c79fb8f49e6d899cf60adfa57044128624
Johann Levesque <johann.levesque@canada.ca>
2020-04-02 15:12:45 -0400

    Updates

--- chart/template.d.ts
+++ chart/template.d.ts
@@ -1,1 +1,2 @@
 export declare const CHART_TEMPLATE = "\n<div class=\"rv-chart-panel\">\n    <canvas id=\"rvChart\" class=\"rv-chart\"></canvas>\n    <div id=\"nouislider\" class=\"rv-chart-slider\"></div>\n</div>";
+export declare const DETAILS_TEMPLATE = "\n<div class=\"rv-chart-details\">\n    <div class=\"rv-chart-details-value\"></div>\n    <md-divider></md-divider>\n</div>\n";
